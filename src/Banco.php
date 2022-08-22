7b905c8d0d27fc2783a956485844ad346dccc8e2
Rafael Augusto <103936985+rafael-tecinternet@users.noreply.github.com>
2022-08-22 11:23:32 -0300

    Programação de conexão com o banco

---
+++ src/Banco.php
@@ -0,0 +1,21 @@
+<?php
+namespace CalorDado;
+use PDO, Exception;
+abstract class Banco {
+    private static string $servidor = "localhost";
+    private static string $usuario = "root";
+    private static string $senha = "";
+    private static string $banco = "calor-dado";
+    private static PDO $conexao; 
+    public static function conecta():PDO{
+        try {
+            self::$conexao = new PDO("mysql:host=".self::$servidor."; dbname=".self::$banco."; charset=utf8", self::$usuario, self::$senha);
+            // Habilida a verificação de erros (em geral e exceções)
+            self::$conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (Exception $erro) {
+            die("Erro: ".$erro->getMessage());
+        }
+        return self::$conexao;
+    }
+}
+?>
