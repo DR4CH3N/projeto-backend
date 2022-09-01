8d93431b36b243bdf79b4034cb4815a0a75f6c62
Rafael Augusto <103936985+rafael-tecinternet@users.noreply.github.com>
2022-09-01 09:18:20 -0300

    Área de admin

---
+++ admin/index.php
@@ -0,0 +1,42 @@
+<?php
+require_once "../inc/cabecalho-admin.php";
+ ?>
+<article class="p-5 my-4 rounded-3 bg-white shadow">
+    <div class="container-fluid py-1">        
+        <h2 class="display-4">Olá <?=$_SESSION['nome']?>!</h2>
+
+        <?php if(isset($_GET['perfil-atualizado'])){?>
+			<p class="my-2 alert alert-primary text-center">
+				Dados atualizados com sucesso!
+			</p>
+        <?php } ?>
+
+        <p class="fs-5">Você está no <b>painel de controle e administração</b> do
+		site Calor Dado e seu <b>nível de acesso</b> é <span class="badge bg-dark"> Admin </span>.</p>
+        <hr class="my-4">
+
+        <div class="d-grid gap-2 d-md-block text-center">
+            <a class="btn btn-dark bg-gradient btn-lg" href="meu-perfil.php">
+                <i class="bi bi-person"></i> <br>
+                Meu perfil
+            </a>
+            <?php  if($_SESSION['tipo'] === 'admin') {  ?>    
+			<a class="btn btn-dark bg-gradient btn-lg" href="categorias.php">
+                <i class="bi bi-tags"></i> <br>
+                Doações
+            </a>
+            <a class="btn btn-dark bg-gradient btn-lg" href="usuarios.php">
+                <i class="bi bi-people"></i> <br>
+                Usuários
+            </a>
+            <?php } ?>
+                                
+        </div>
+    </div>
+</article>
+
+
+<?php 
+require_once "../inc/rodape.php";
+?>
+
--- inc/cabecalho-admin.php
+++ inc/cabecalho-admin.php
@@ -1,45 +1,73 @@
+<?php
+ob_start();
+use CalorDado\ControleDeAcesso;
+require_once "../vendor/autoload.php";
+/* Criamos objeto para acessar os recursos de sessão PHP na classe ControleDeAcesso */
+$sessao = new ControleDeAcesso;
+/* Executamos VerificaAcesso para checar se tem alguém logado */
+/* $sessao->verfificaAcesso(); */
+/* Se o parâmetro ?sair existir, então faça o logout */
+if(isset($_GET['sair'])){
+    $sessao->logout();
+}
+$pagina = basename($_SERVER['PHP_SELF']);
+?>
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-br" class="h-100">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Calor Dado</title>
-   
-  <!-- Favicon -->
-  <link rel="shortcut icon" href="img/img-logos/Favicon_png-min.png">
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Calor Dado</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+<link rel="stylesheet" href="../css/style.css">
 
-  <!-- Descrição resumida da página -->
-  <meta name="description" content="Somos uma instituição que busca ajudar as pessoas de extrema necessidade que moram nas ruas e passam por situações difícil, ainda mais em tempo de inverno.">
-    
-  <!-- Palavras-chave da página -->
-  <meta name="keywords" content="Doação, doação de agasalhos, doação de calçados">
-  
-  <!-- Inserindo o Bootstrap-->
-  <link rel="stylesheet" href="bootstrap-5.2.0-beta1-dist/bootstrap-5.2.0-beta1-dist/css/bootstrap.css">
-
-  <!-- Linkando para CSS externo-->
-  <link rel="stylesheet" href="../style.css">
 </head>
-<body>
+<body id="admin" class="d-flex flex-column h-100 bg-white bg-gradient">
+    
+<header id="topo" class="border-bottom sticky-top">
 
-  <!-- Início do cabeçalho --> 
-  <header >
-    <div class="limitador">
-      <a href="../index.html" title="Página Inicial"><img src="../img/img-logos/logo-calor-dado-min.png" alt="Calor Dado"></a>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark justify-content-between">
+  <div class="container">
+    <h1><a class="navbar-brand" href="index.php"><i class="bi bi-unlock"></i> Admin | Calor Dado</a></h1>
 
-    <!-- Menu de navegação -->
-      <nav>
-        <h2 class="icone"><a href="" title="Abra menu de navegação">Menu &equiv;</a></h2>
-          <ul class="menu"></a>
-            <li><a href="../index.html" title="página inicial">Home</a></li>
-            <li><a href="../quemsomos.html" title="página quem somos">QUEM SOMOS</a></li>
-          <li><a href="../querodoar.php" title="página quero doar">QUERO DOAR</a></li>
-          <li><a href="../contato.html" title="página contato">CONTATO</a></li>
-          </ul>
-          <li><a href="../login.php" title="Entrar">Entrar</a></li>
-          </ul>
-      </nav>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="meu-perfil.php">Meu perfil</a>
+            </li>
+            <?php if($_SESSION['tipo'] === 'admin') { ?>
+            <li class="nav-item">
+                <a class="nav-link" href="categorias.php">Doações</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="usuarios.php">Usuários</a>
+            </li>            
+            <?php }  ?>
+            <li class="nav-item">
+                <a class="nav-link fw-bold" href="?sair"> <i class="bi bi-x-circle"></i> Sair</a>
+            </li>
+
+         
+             
+        </ul>
+
     </div>
-  </header>
-  <!-- Fim do cabeçalho -->
+  </div>
+</nav>
+
+</header>
+
+<main class="flex-shrink-0">
+    <div class="container">
+
+    
