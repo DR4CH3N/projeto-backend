94b8957a28fdfc2425efdf2f0f4863a3546ed856
Rafael Augusto <103936985+rafael-tecinternet@users.noreply.github.com>
2022-09-10 09:57:34 -0300

    Add files via upload

--- banco-de-dados/calor-dado-novo.sql
+++ banco-de-dados/calor-dado-novo.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 29-Ago-2022 às 16:56
--- Versão do servidor: 10.4.22-MariaDB
--- versão do PHP: 8.0.13
+-- Tempo de geração: 10-Set-2022 às 14:56
+-- Versão do servidor: 10.4.24-MariaDB
+-- versão do PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,54 +18,49 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `calor-dado`
+-- Banco de dados: `calor-dado-novo`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `admin`
+-- Estrutura da tabela `cadastros`
 --
 
-CREATE TABLE `admin` (
+CREATE TABLE `cadastros` (
   `id` int(11) NOT NULL,
-  `email` varchar(45) NOT NULL,
-  `senha` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Extraindo dados da tabela `admin`
---
-
-INSERT INTO `admin` (`id`, `email`, `senha`) VALUES
-(1, 'matheus@gmail.com', '$2y$10$IEDUAeC3tpo6RTFuCJg06OS8rBdeKcOWmh.G8ikocslm6hckagc/K'),
-(2, 'Antônio', '$2y$10$kRRC9fXuHtny239KqDaJo.kL/HirUb9fm7bzwjoIWGCt24/GmPpdO');
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `cadastro`
---
-
-CREATE TABLE `cadastro` (
-  `id` int(11) NOT NULL,
-  `endereco` varchar(80) NOT NULL,
-  `cep` varchar(8) NOT NULL,
-  `cidade` varchar(45) NOT NULL,
-  `numero` varchar(5) NOT NULL,
-  `complemento` varchar(30) NOT NULL,
-  `bairro` varchar(30) NOT NULL,
+  `telefone` varchar(14) NOT NULL,
+  `endereco` varchar(20) NOT NULL,
+  `cep` varchar(9) NOT NULL,
+  `cidade` varchar(20) NOT NULL,
+  `numero` varchar(20) NOT NULL,
+  `bairro` varchar(20) NOT NULL,
+  `complemento` varchar(20) NOT NULL,
   `usuario_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Extraindo dados da tabela `cadastro`
+-- Extraindo dados da tabela `cadastros`
 --
 
-INSERT INTO `cadastro` (`id`, `endereco`, `cep`, `cidade`, `numero`, `complemento`, `bairro`, `usuario_id`) VALUES
-(5, 'Rua nove de julho', '08505-00', 'Ferraz de Vasconcelos', '1000', '', '', 2),
-(6, 'Rua nove de novembro', '0800-000', 'São Paulo', '1500', '', '', 1),
-(9, 'Rua da 25 de março', '08505-00', 'São Paulo', '0000', '900/915', 'josefina', 3);
+INSERT INTO `cadastros` (`id`, `telefone`, `endereco`, `cep`, `cidade`, `numero`, `bairro`, `complemento`, `usuario_id`) VALUES
+(14, '(19) 69520-059', 'Rua David Livingston', '03801-050', 'São Paulo', '1', 'Parque Boturussu', '', 2),
+(15, '(19) 69520-059', 'Rua David Livingston', '03801-050', 'São Paulo', '1', 'Parque Boturussu', '', 2),
+(16, '(19) 6952-0059', 'Rua Professor José d', '03801-010', 'São Paulo', '1', 'Parque Boturussu', '', 2),
+(17, '(19) 6952-0059', 'Rua Professor José d', '03801-010', 'São Paulo', '1', 'Parque Boturussu', '', 2),
+(18, '(19) 6952-0059', 'Rua David Livingston', '03801-050', 'São Paulo', '1', 'Parque Boturussu', '', 2),
+(19, '(19) 6952-0059', 'Rua David Livingston', '03801-050', 'São Paulo', '1', 'Parque Boturussu', '', 2),
+(20, '(19) 6952-0059', 'Rua David Livingston', '03801-050', 'São Paulo', '1', 'Parque Boturussu', '', 2),
+(21, '(19) 69520-059', 'Rua David Livingston', '03801-050', 'São Paulo', '1', 'Parque Boturussu', '', 3),
+(22, '', 'Rua David Livingston', '03801-050', 'São Paulo', '1', 'Parque Boturussu', '', 3),
+(23, '(19) 69520-059', 'Rua Professor José d', '03801-010', 'São Paulo', '', 'Parque Boturussu', '', 3),
+(24, '', 'Rua Professor José d', '03801-010', 'São Paulo', '', 'Parque Boturussu', '', 3),
+(25, '(19) 69520-059', 'Rua Professor José d', '03801-010', 'São Paulo', '', 'Parque Boturussu', '', 2),
+(26, '', 'Rua David Livingston', '03801-050', 'São Paulo', '1', 'Parque Boturussu', '', 2),
+(27, '', 'Rua David Livingston', '03801-050', 'São Paulo', '1', 'Parque Boturussu', '', 2),
+(28, '(19) 69520-059', 'Rua David Livingston', '03801-050', 'São Paulo', '', 'Parque Boturussu', '', 2),
+(29, '(19) 69520-059', 'Rua David Livingston', '03801-050', 'São Paulo', '', 'Parque Boturussu', '', 2),
+(30, '(19) 69520-059', 'Rua David Livingston', '03801-050', 'São Paulo', '', 'Parque Boturussu', '', 2);
 
 -- --------------------------------------------------------
 
@@ -75,20 +70,35 @@ INSERT INTO `cadastro` (`id`, `endereco`, `cep`, `cidade`, `numero`, `complement
 
 CREATE TABLE `doacoes` (
   `id` int(11) NOT NULL,
-  `doacao` set('roupas','cobertores','calcados') NOT NULL,
-  `Quantidade` int(11) NOT NULL,
-  `usuario_id` int(11) NOT NULL
+  `roupas` int(11) NOT NULL,
+  `calcados` int(11) NOT NULL,
+  `cobertores` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `pix` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Extraindo dados da tabela `doacoes`
 --
 
-INSERT INTO `doacoes` (`id`, `doacao`, `Quantidade`, `usuario_id`) VALUES
-(8, 'calcados', 2, 1),
-(9, 'roupas,cobertores', 10, 2),
-(10, 'roupas', 4, 2),
-(11, 'roupas,cobertores,calcados', 6, 1);
+INSERT INTO `doacoes` (`id`, `roupas`, `calcados`, `cobertores`, `usuario_id`, `pix`) VALUES
+(14, 0, 0, 0, 2, ''),
+(15, 0, 0, 0, 2, ''),
+(16, 1, 0, 1, 2, ''),
+(17, 1, 0, 1, 2, ''),
+(18, 1, 0, 0, 2, ''),
+(19, 1, 0, 1, 2, ''),
+(20, 1, 0, 1, 2, ''),
+(21, 0, 0, 0, 3, ''),
+(22, 0, 0, 0, 3, ''),
+(23, 0, 0, 0, 3, ''),
+(24, 0, 0, 0, 3, ''),
+(25, 0, 0, 0, 2, ''),
+(26, 0, 0, 1, 2, ''),
+(27, 0, 0, 1, 2, ''),
+(28, 1, 0, 1, 2, ''),
+(29, 1, 1, 1, 2, ''),
+(30, 1, 1, 1, 2, '');
 
 -- --------------------------------------------------------
 
@@ -98,38 +108,31 @@ INSERT INTO `doacoes` (`id`, `doacao`, `Quantidade`, `usuario_id`) VALUES
 
 CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
-  `nome` varchar(45) NOT NULL,
-  `email` varchar(45) NOT NULL,
+  `nome` varchar(20) NOT NULL,
+  `email` varchar(20) NOT NULL,
   `senha` varchar(255) NOT NULL,
-  `Tipo` enum('admin','usuario') NOT NULL
+  `tipo` enum('admin','usuario') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Extraindo dados da tabela `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `Tipo`) VALUES
-(1, 'Matheus', 'matheus123@gmail.com', '$2y$10$Ij1ovHGmnFgJ.0y1J4/A6.6wTewS44W42e7e59.bZYzG5u0OIkmte', 'admin'),
-(2, 'Lucas', 'lucas@gmail.com', '$2y$10$GTzS/heiBjX0ZU8qEcei5OKuRpFYlvLkG/aUyypAmSu/KoPyU6GUS', 'admin'),
-(3, 'Thiago', 'thiago@gmail.com', '$2a$12$4rbzN3NAyyY67ScgEOS1Tu6fPPSlV.2w1iniJTlIoVkrszKdt0o2.', 'admin');
+INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `tipo`) VALUES
+(1, 'rafa', 'rafa@gmail.com', '$2y$10$oR10HntgYGNGkrIAk84ZGuscmpmHcj5zOiqjWy.ZmQ0DEuOaXhjFC', 'admin'),
+(2, 'gi', 'gi@gmail.com', '$2y$10$pLC8zucg9R01/DT9rtwsH.5QrKwXwgPjJGG33OddYhC03FImTGnbW', 'usuario'),
+(3, 'juca', 'juca@gmail.com', '$2y$10$7.XTeljGR4Gbf52FL6s77ObIUPOdZXbu30Q3NmxU3oOGwDLL87wpG', 'usuario');
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices para tabela `admin`
+-- Índices para tabela `cadastros`
 --
-ALTER TABLE `admin`
+ALTER TABLE `cadastros`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- Índices para tabela `cadastro`
---
-ALTER TABLE `cadastro`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_cadastro_usuarios` (`usuario_id`) USING BTREE;
+  ADD KEY `usuario_id` (`usuario_id`);
 
 --
 -- Índices para tabela `doacoes`
@@ -142,46 +145,39 @@ ALTER TABLE `doacoes`
 -- Índices para tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
--- AUTO_INCREMENT de tabela `admin`
+-- AUTO_INCREMENT de tabela `cadastros`
 --
-ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT de tabela `cadastro`
---
-ALTER TABLE `cadastro`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+ALTER TABLE `cadastros`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT de tabela `doacoes`
 --
 ALTER TABLE `doacoes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Restrições para despejos de tabelas
 --
 
 --
--- Limitadores para a tabela `cadastro`
+-- Limitadores para a tabela `cadastros`
 --
-ALTER TABLE `cadastro`
-  ADD CONSTRAINT `fk_contato_meu_perfil_usuarios` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
+ALTER TABLE `cadastros`
+  ADD CONSTRAINT `cadastros_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
 
 --
 -- Limitadores para a tabela `doacoes`
